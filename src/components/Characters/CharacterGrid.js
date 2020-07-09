89b66156f7f7a64a8f056c79617e5f79641121de
William Vegas <elkareem123@gmail.com>
2020-07-09 16:14:37 -0400

    'primer-commit-bb'

---
+++ src/components/Characters/CharacterGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//componentes
+import CharacterItem from "./CharacterItem"
+import Spinner from "../UI/Spinner"
+
+const CharacterGrid = ( {items, isLoading} ) => {
+    return isLoading ? <Spinner /> : <section className="cards">
+      {items.map(item => (
+        <CharacterItem key={item.char_id} item={item} />
+      ))}
+    </section>
+};
+
+export default CharacterGrid;
